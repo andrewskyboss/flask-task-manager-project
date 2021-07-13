0d9a7b61a4b58ea496e836f2f77d5516f24ad5ce
andrewskyboss <andrew.cybossky@gmail.com>
2021-07-13 23:47:53 +0000

    bind data from db to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,7 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
 });
 
 
@@ -18,4 +19,8 @@ $(document).ready(function () {
 // document.addEventListener('DOMContentLoaded', function () {
 //     let sidenavs = document.querySelectorAll(".sidenav");
 //     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+//     let collapsibles = document.querySelectorAll(".collapsible");
+//     let collapsiblesInstance = M.Collapsible.init(collapsibles);
+//     let tooltips = document.querySelectorAll(".tooltipped");
+//     let tooltipsInstance = M.Tooltip.init(tooltips);
 // });
