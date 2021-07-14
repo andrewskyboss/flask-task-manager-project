1edb0423d44ce9a78c0f37a912022c260db2b925
andrewskyboss <andrew.cybossky@gmail.com>
2021-07-14 23:17:13 +0000

    update

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-  
 import os
 from flask import (
     Flask, flash, render_template,
@@ -117,6 +116,13 @@ def add_task():
 
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_task.html", categories=categories)
+
+
+@app.route("/edit_task/<task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("edit_task.html", task=task, categories=categories)
 
 
 if __name__ == "__main__":
